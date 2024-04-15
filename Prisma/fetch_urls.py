2f32029f83200ada71fae6fdde6b632d68df6bc6
AntonJ <antonjastsuk66@gmail.com>
2024-04-15 15:02:32 +0300

    Structure changes and import changes

--- Prisma/fetch_urls.py
+++ Prisma/fetch_urls.py
@@ -1,16 +1,16 @@
-from web_scraper_helper import *
+import Prisma.helper as wb
 
-async def get_urls_list(current_page: str, session: aiohttp.ClientSession, progress: Spinner) -> list:
+async def get_urls_list(current_page: str, session: wb.aiohttp.ClientSession, progress: wb.Spinner) -> list:
     
     try:
         async with session.get(current_page) as response:
             
             if response.status != 200:
-                raise aiohttp.ClientResponseError
+                raise wb.aiohttp.ClientResponseError
 
             html = await response.text()
 
-            selection_html = BeautifulSoup(html, 'html.parser')
+            selection_html = wb.BeautifulSoup(html, 'html.parser')
 
             # Getting category names
             categories = selection_html.find_all('a', class_ = 'name js-category-item')
@@ -24,14 +24,14 @@ async def get_urls_list(current_page: str, session: aiohttp.ClientSession, progr
 
             return links_list
         
-    except aiohttp.ClientResponseError as e:
+    except wb.aiohttp.ClientResponseError as e:
         print(f"Error fetching {current_page}: {e}")
         return None
 
-async def main(main_page: str, progress: Spinner):
+async def main(main_page: str, progress: wb.Spinner):
 
     # Use a single session for connection pooling to reduce requests time
-    async with aiohttp.ClientSession() as session:
+    async with wb.aiohttp.ClientSession() as session:
 
         # Get categories urls list
         categories_urls = await get_urls_list(main_page, session, progress)
@@ -40,7 +40,7 @@ async def main(main_page: str, progress: Spinner):
         tasks = [get_urls_list(category_url, session, progress) for category_url in categories_urls]
         
         # Wait for all coroutines to finish
-        results = await asyncio.gather(*tasks)
+        results = await wb.asyncio.gather(*tasks)
         
         return results
 
@@ -48,9 +48,9 @@ def fetch_urls():
     # Prisma website product selection
     prisma_selection = 'https://www.prismamarket.ee/products/selection'
 
-    progress = Spinner('Fetching urls ')
+    progress = wb.Spinner('Fetching urls ')
 
-    subcategories_urls = asyncio.run(main(prisma_selection, progress))
+    subcategories_urls = wb.asyncio.run(main(prisma_selection, progress))
 
     progress.finish()
 
@@ -59,4 +59,4 @@ def fetch_urls():
     urls = [url for subcategory in subcategories_urls for url in subcategory]
 
     with open('data/urls.json', 'w') as urls_file:
-        json.dump(urls, urls_file, indent=4)
+        wb.json.dump(urls, urls_file, indent=4)
--- Prisma/scrape_products.py
+++ Prisma/scrape_products.py
@@ -1,34 +1,34 @@
-from web_scraper_helper import *
+import Prisma.helper as wb
 from fetch_urls import fetch_urls
 
-async def collect_products(html: BeautifulSoup, products_left: int, url: str, session: aiohttp.ClientSession):
+async def collect_products(html: wb.BeautifulSoup, products_left: int, url: str, session: wb.aiohttp.ClientSession):
 
     # Save a list of dictionaries
-    products = get_products_list('div', 'info relative clear', html)
+    products = wb.get_products_list('div', 'info relative clear', html)
     
     products_per_page = len(products)
 
     # If more products left, open next page, scrape products until all are done, append to the end of the list
     if products_left > products_per_page:
         products_left -= products_per_page
-        url = next_page(url)
+        url = wb.next_page(url)
         async with session.get(url) as response:
 
             if response.status != 200:
                 print(f'Error at {url} status: {response.status}.')
-                raise aiohttp.ClientResponseError
+                raise wb.aiohttp.ClientResponseError
 
             html = await response.text()
-            html = BeautifulSoup(html, 'html.parser')
+            html = wb.BeautifulSoup(html, 'html.parser')
             add_products = await collect_products(html, products_left, url, session)
             products.extend(add_products)
 
     return products
 
-async def get_products_from_html(html, url: str, session: aiohttp.ClientSession):
-    products_html = BeautifulSoup(html, 'html.parser')
+async def get_products_from_html(html, url: str, session: wb.aiohttp.ClientSession):
+    products_html = wb.BeautifulSoup(html, 'html.parser')
 
-    all_products_count = get_products_total(products_html)
+    all_products_count = wb.get_products_total(products_html)
 
     # Collect products and open new url pages if not all products collected
     products = await collect_products(products_html, all_products_count, url, session)
@@ -36,19 +36,19 @@ async def get_products_from_html(html, url: str, session: aiohttp.ClientSession)
     return products
     
 
-async def get_page_products(current_page: str, session: aiohttp.ClientSession, progress: Bar):
+async def get_page_products(current_page: str, session: wb.aiohttp.ClientSession, progress: wb.Bar):
     
     try:
         async with session.get(current_page) as response:
 
             if response.status != 200:
                 print(f'Error at {current_page} status: {response.status}.')
-                raise aiohttp.ClientResponseError
+                raise wb.aiohttp.ClientResponseError
 
             html = await response.text()
             products = await get_products_from_html(html, current_page, session)
 
-    except aiohttp.ClientResponseError as e:
+    except wb.aiohttp.ClientResponseError as e:
         print(f'Error in connection: {str(e)}')
         return None
         
@@ -57,13 +57,13 @@ async def get_page_products(current_page: str, session: aiohttp.ClientSession, p
     return products
 
 async def main(urls: list, progress):
-    async with aiohttp.ClientSession() as session:
+    async with wb.aiohttp.ClientSession() as session:
         
         # Use a single session for connection pooling
         tasks = [get_page_products(url, session, progress) for url in urls]
         
         # Wait for all coroutines to finish
-        results = await asyncio.gather(*tasks)
+        results = await wb.asyncio.gather(*tasks)
         
         return results
 
@@ -73,19 +73,19 @@ if __name__ == '__main__':
     urls_file = 'data/urls.json'
 
     # Check if the file with products already exists
-    if os.path.exists(products_file) and cmp_dates(products_file):
+    if wb.os.path.exists(products_file) and wb.cmp_dates(products_file):
             print('File exists and dates are the same.')
             exit()
     # Only create a file if it doesnt exist and the date is different
     else:
         # If urls file doesnt exist, fetch urls
-        if not os.path.exists(urls_file):
+        if not wb.os.path.exists(urls_file):
             fetch_urls()
-        urls, all_urls_count = get_urls_and_len(urls_file)
+        urls, all_urls_count = wb.get_urls_and_len(urls_file)
 
-        progress = Bar('Collecting products', max=all_urls_count, suffix='%(percent)d%%')
+        progress = wb.Bar('Collecting products', max=all_urls_count, suffix='%(percent)d%%')
 
-        data = asyncio.run(main(urls, progress))
+        data = wb.asyncio.run(main(urls, progress))
 
         # Flat out data, taking each product from the list of lists and
         # Putting into one list
@@ -94,4 +94,4 @@ if __name__ == '__main__':
         progress.finish()
         print('Finished scraping products from Prisma')
 
-        save_json(products_file, data)
+        wb.save_json(products_file, data)
